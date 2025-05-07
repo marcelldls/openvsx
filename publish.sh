4a137fd9ec03b823d87e516334644285f51e1318
Marcell Douglas Nagy <133755508+marcelldls@users.noreply.github.com>
2025-05-07 16:54:23 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -2,3 +2,4 @@
 npm install ovsx
 npx ovsx create-namespace MyOrg --registryUrl http://localhost:8080 -p super_token
 npx ovsx publish --registryUrl http://localhost:8080 -p super_token myExtension.vsix
+# docker run -it -p 3001:3000 -e VSX_REGISTRY_URL=http://localhost:8080 ghcr.io/eclipse-theia/theia-ide/theia-ide
