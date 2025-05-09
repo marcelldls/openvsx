38237565beada8c58eb9821b6dbdef2c85249ce3
Marcell Douglas Nagy <133755508+marcelldls@users.noreply.github.com>
2025-05-09 09:33:19 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-# docker compose --profile backend --profile db --profile es up -d
+# docker compose --profile backend --profile db --profile es up
 npm install ovsx
 npx ovsx create-namespace MyOrg --registryUrl http://localhost:8080 -p super_token
 npx ovsx publish --registryUrl http://localhost:8080 -p super_token myExtension.vsix
